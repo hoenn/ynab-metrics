cc0f02160be8eef9aae9661d1ad8380a9181ddfd
Evan Mena <evan.mena13@gmail.com>
2018-09-28 17:14:46 -0400

    Added flag for token, makefile, target

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"flag"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var addr = flag.String("listen-address", ":8080", "The address to lsiten on for HTTP requests")
+var token = flag.String("token", "", "Your YNAB access token")
 
 func main() {
+	flag.Parse()
+	if *token == "" {
+		panic("Must run with --token=abc... flag")
+	}
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
